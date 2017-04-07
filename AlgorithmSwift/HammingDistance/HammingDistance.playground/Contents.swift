0e8ac7dfa1927c2c9c232e38f68437ddf082fa69
Kevin Fan <hotings@Kevins-MacBook-Pro.local>
2017-04-06 20:31:29 -0600

    Update the test cases

--- AlgorithmSwift/HammingDistance/HammingDistance.playground/Contents.swift
+++ AlgorithmSwift/HammingDistance/HammingDistance.playground/Contents.swift
@@ -2,14 +2,17 @@
 
 import Foundation
 
-let input1 = 1
-let input2 = 4
-
-let distance = hammingDistance(input1, input2)
-
-print(distance)
-
 func hammingDistance(_ x: Int, _ y: Int) -> Int {
     let c = x ^ y
     return String(c, radix: 2).characters.filter{ $0 == "1"}.count
 }
+
+let input1 = 1
+let input2 = 4
+
+hammingDistance(input1, input2)
+
+
+
+
+
