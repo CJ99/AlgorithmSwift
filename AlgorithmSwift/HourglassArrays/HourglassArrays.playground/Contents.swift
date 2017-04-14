2643cd65fe9e56094f2a66fc103b2c6aeae8f974
CJ99 <xf2@ualberta.ca>
2017-04-13 23:14:31 -0600

    Add Hourglass Arrays (2D)

---
+++ AlgorithmSwift/HourglassArrays/HourglassArrays.playground/Contents.swift
@@ -0,0 +1,32 @@
+//: Playground - noun: a place where people can play
+
+import Foundation
+
+var rows = 6, cols = 6
+var dimensional = Array<Array<String>>()
+
+for col in 0..<6 {
+    let numStrings = readLine()!
+    dimensional.append(Array(numStrings.components(separatedBy: " ")))
+}
+
+var n = 0
+var count = 4
+var sum = [Int]()
+
+while (count > n) {
+    var m = 0
+    while (count > m) {
+        sum.append (Int(dimensional[n][m])! + Int(dimensional[n][m+1])! + Int(dimensional[n][m+2])!
+            + Int(dimensional[n+1][m+1])!
+            + Int(dimensional[n+2][m+0])! + Int(dimensional[n+2][m+1])! + Int(dimensional[n+2][m+2])!)
+        
+        m += 1
+    }
+    n += 1
+    
+}
+
+
+
+print(sum.max()!)
---
+++ AlgorithmSwift/HourglassArrays/HourglassArrays.swift
@@ -0,0 +1,45 @@
+//
+//  Hourglass Arrays.swift
+//  AlgorithmSwift
+//
+//  Created by Kevin Fan on 2017-04-13.
+//  Copyright © 2017 Kevin Fan. All rights reserved.
+//
+//  1 1 1 0 0 0
+//  0 1 0 0 0 0
+//  1 1 1 0 0 0
+//  0 0 2 4 4 0
+//  0 0 0 2 0 0
+//  0 0 1 2 4 0
+//
+
+import Foundation
+
+//var rows = 6, cols = 6
+//var dimensional = Array<Array<String>>()
+//
+//for col in 0..<6 {
+//    let numStrings = readLine()!
+//    dimensional.append(Array(numStrings.components(separatedBy: " ")))
+//}
+//
+//var n = 0
+//var count = 4
+//var sum = [Int]()
+//
+//while (count > n) {
+//    var m = 0
+//    while (count > m) {
+//        sum.append (Int(dimensional[n][m])! + Int(dimensional[n][m+1])! + Int(dimensional[n][m+2])!
+//            + Int(dimensional[n+1][m+1])!
+//            + Int(dimensional[n+2][m+0])! + Int(dimensional[n+2][m+1])! + Int(dimensional[n+2][m+2])!)
+//        
+//        m += 1
+//    }
+//    n += 1
+//    
+//}
+//
+//
+//
+//print(sum.max()!)
